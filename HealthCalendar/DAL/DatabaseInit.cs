7d042a42b4fc0dc5aaf5b051d007c846228def0b
stian <65687147+sti-and@users.noreply.github.com>
2025-10-26 02:42:00 +0200

    Push x2

--- HealthCalendar/DAL/DatabaseInit.cs
+++ HealthCalendar/DAL/DatabaseInit.cs
@@ -2,8 +2,6 @@ using Microsoft.EntityFrameworkCore;
 namespace HealthCalendar.DAL;
 using HealthCalendar.Models;
 
-
-
 public static class DatabaseInit
 {
     public static void Seed(IApplicationBuilder app)
